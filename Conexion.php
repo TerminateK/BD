0d7ec6ac1b3fba3646158339d0e6d069bf8d0be5
TerminateK <maurog2000@gmail.com>
2020-12-19 16:56:23 -0300

    Conexion a base de datos

---
+++ Conexion.php
@@ -0,0 +1,13 @@
+<?php
+
+$link='mysql:host=localhost;dbname=bd';
+$user='root';
+$pw='';
+
+try{
+    $pdo= new PDO($link,$user,$pw);
+}catch (PDOException $e) {
+    print "¡Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
