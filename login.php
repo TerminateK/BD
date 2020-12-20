8ff61741fb8e9ba2d2af7bf2f34b995072f4c109
JavierFuentes1 <panyi@Javier.localdomain>
2020-12-19 22:45:24 -0300

    q wea

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Bienvenido</title>
+        <h1>
+           Ingrese sus datos de ciudadano
+        </h1>
+    </head>
+    <body>
+        <form action="login.php" method="post">
+            <input type="text" name="nombre" placeholder="Ingrese su nombre">
+            <input type="id" name="id_persona" placeholder= "Ingrese su ID">
+            <input type="submit" name="Enviar"
+        </form>
+    </body>
+
+</html>
