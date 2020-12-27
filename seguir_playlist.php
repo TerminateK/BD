4af523da6da533b0b4be4a56a5a131452c6d41f9
JavierFuentes1 <70865004+JavierFuentes1@users.noreply.github.com>
2020-12-26 23:26:01 -0300

    commit

---
+++ seguir_playlist.php
@@ -0,0 +1,22 @@
+<?php
+include_once ("Conexion.php");
+session_start();
+
+if(isset($_SESSION['ID_Cuenta'])){
+    $cuenta = $_REQUEST['id_cuenta'];
+    $lista = $_REQUEST['id_lista'];
+    $query = $pdo->query("SELECT * FROM seguidos_lista WHERE id_lista = '$lista' and id_cuenta = '$cuenta'")->fetch();
+
+    if ($query) {
+        header("Location:myprofile.php?");
+    }
+    $create = 'INSERT INTO seguidos_lista (id_cuenta,id_lista) VALUES (?,?)';
+    $agregar = $pdo->prepare($create);
+    $agregar->execute(array($cuenta,$lista));
+    header("Location:myprofile.php");
+
+}
+
+
+
+
