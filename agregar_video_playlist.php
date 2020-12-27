aaa47008bac31995a31197c29339fed69f112a4c
JavierFuentes1 <70865004+JavierFuentes1@users.noreply.github.com>
2020-12-26 22:46:16 -0300

    Conexion a base de datos

--- agregar_video_playlist.php
+++ agregar_video_playlist.php
@@ -5,10 +5,15 @@
     if(isset($_SESSION['ID_Cuenta'])){
         $video = $_REQUEST['id_video'];
         $lista = $_REQUEST['id_lista'];
+        $query = $pdo->query("SELECT * FROM lista_video WHERE id_lista = '$lista' and id_video = '$video'")->fetch();
+
+        if ($query) {
+            header("Location:mostrarlistas.php?id_video=$lista");
+        }
         $create = 'INSERT INTO lista_video (id_video,id_lista) VALUES (?,?)';
         $agregar = $pdo->prepare($create);
         $agregar->execute(array($video,$lista));
-        // header("Location:mostrarlistas.php?id_video=$lista");
+        header("Location:mostrarlistas.php?id_video=$lista");
 
     }
 
